ac9f05edc6b7c7277917de85b995b6aa2f2c2e87
Pete Saia <pete@petesaia.com>
2018-09-30 12:32:47 -0700

    Update start-dev.sh

--- start-dev.sh
+++ start-dev.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 go build
 
+# Redis Credentials
 export REDIS_DB="0"
 export REDIS_SERVER="localhost:6379"
 export REDIS_PASS=""
+
+# Port for the public application to listen.
 export APP_PORT=":8000"
+
+# Port for the internal gRPC server to listen.
 export GRPC_PORT=":8001"
+
+# The starting point for the short code algorithm.
 export CHAR_FLOOR="2"
+
+# URL to redirect to if the link isn't found.
 export NOTFOUND_REDIRECT_URL="https://github.com"
+
+# Full URL for the public dwarf app to run.
+# No trailing slash.
 export APP_BASE_URL="http://localhost:8000"
+
 ./dwarf
